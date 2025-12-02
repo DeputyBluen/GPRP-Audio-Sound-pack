213d1b907777eec1b9a622e63a4388b2228aaf1c
DeputyBluen <39847145+DeputyBluen@users.noreply.github.com>
2025-12-02 15:19:36 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -631,8 +631,8 @@ data_file 'AUDIO_SOUNDDATA' 'audioconfig/cvpiv8_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_cvpiv8'
 
 --EcoBoost V6--
-data_file 'AUDIO_GAMEDATA' 'audioconfig/ecoboostv6_game.dat151.rel'
-data_file 'AUDIO_SOUNDDATA' 'audioconfig/ecoboostv6_sounds.dat54.rel'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ecoboostv6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ecoboostv6_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ecoboostv6'
 
 --LTChevy--
